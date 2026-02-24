c2b831a743e78311436f687fa4a0df45b265aa02
manibharathi2303 <mani.bharathi.2303@gmail.com>
2026-02-24 22:36:58 +0530

    CI/CD test

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const seedBillSettings = require('./seeders/billSettingSeeder');
 const seedTriggers = require('./seeders/triggerSeeder');
 
 const PORT = process.env.PORT || 5000;
-
+console.log("🚀 CI/CD Test: Deployment Successful!");
 
 const start = async () => {
     try {
@@ -27,4 +27,3 @@ const start = async () => {
 };
 
 start();
-console.log("🚀 CI/CD Test: Deployment Successful!");
